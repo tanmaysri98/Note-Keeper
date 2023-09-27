57d9a8bf3d0d85103944613f4eec339deb328a4a
Tanmay Srivastava <rahulkumar4951@gmail.com>
2023-09-27 15:47:42 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Portfolio</h1>
+      <img src="https://unsplash.com/photos/62V7ntlKgL8" alt="Background Northern Lights" />
+    </header>
+  );
+};
+
+export default Header;
